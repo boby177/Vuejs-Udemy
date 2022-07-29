89aa6531ccdf7b8c360af2f7ee1f2dc348a64de4
Boby Subagja <boby.subagja@wgs-id.com>
2022-07-29 16:16:09 +0700

    Section 3 & 4 learn about component of vue js

---
+++ Section-4/my-first-project/demo/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <!-- <PostsExample/> -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <router-link class="nav-link" :to="'/'">Home
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" :to="'/contact'">Contact
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import PostsExample from './components/Posts'
+import Contact from './components/Contact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PostsExample,
+    Contact
+  }
+}
+</script>
+
+<style>
+  /* import bootstrap lib */
+  @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
---
+++ Section-4/my-first-project/demo/src/components/Posts.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mt-3">
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <!-- <th scope="col">User Id</th> -->
+      <th scope="col">Id</th>
+      <th scope="col">Title</th>
+      <th scope="col">Body</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="post in posts" :key="post.id">
+      <!-- <th scope="row">1</th> -->
+      <!-- <td>{{post.userId}}</td> -->
+      <td>{{post.id}}</td>
+      <td>{{post.title}}</td>
+      <td>{{post.body}}</td>
+    </tr>
+  </tbody>
+</table>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "PostsExample",
+
+  data() {
+    return {
+      posts: [
+        
+      ]
+    };
+  },
+
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(posts => {
+            this.posts = posts.data
+        })
+    }
+};
+</script>
+
+<style></style>
